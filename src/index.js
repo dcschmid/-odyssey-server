e46a9aadd0f9ccda3a69fa72cbe736915a96fb47
Daniel Schmid <daniel.schmid@mailbox.org>
2022-02-12 22:55:47 +0100

    fix: set port

--- src/index.js
+++ src/index.js
@@ -3,18 +3,23 @@ const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 const TrackAPI = require("./datasources/track-api");
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => {
-    return { trackAPI: new TrackAPI() };
-  },
-});
+async function startApolloServer(typeDefs, resolvers) {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+      return {
+        trackAPI: new TrackAPI(),
+      };
+    },
+  });
 
-server.listen({ port: process.env.PORT || 4000 }).then(() => {
+  const { url, port } = await server.listen({ port: process.env.PORT || 4000 });
   console.log(`
-    Server is running!
-    Listening on port 4000
-    Query at https://studio.apollographql.com/dev
+      🚀  Server is running
+      🔉  Listening on port ${port}
+      📭  Query at ${url}
     `);
-});
+}
+
+startApolloServer(typeDefs, resolvers);
